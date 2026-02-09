455496fd933e84697a58e9d3498b7baf3ae17796
Ewan-Dev <ewanmccairn2010@gmail.com>
2026-02-09 21:53:08 +0000

    feat(processing): convert rgb image to luminoscity greyscale

--- software/image-processing/grayscale.py
+++ software/image-processing/grayscale.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 
 def convert_image_to_RGB(path):
     img = Image.open(path)
@@ -13,3 +14,18 @@ def convert_image_to_RGB(path):
             row.append(pixel)
         img_array.append(row)
     return img_array
+
+# using luminosity greyscale for better representation 
+def convert_RGB_to_luminosity_greyscale(rgb_img_array):
+    greyscale_img_array = []
+    for row in rgb_img_array:
+        new_row = []
+        for pixel in row:
+            red = pixel[0] * 0.21
+            green = pixel[1] * 0.72
+            blue = pixel[2] * 0.07
+            new_pixel = red + green + blue
+            new_row.append((new_pixel, new_pixel, new_pixel))
+        greyscale_img_array.append(new_row)
+    return greyscale_img_array
+
