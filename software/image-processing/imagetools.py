e7dd4f82c888fb6c8fa72271f6026684020954e5
Ewan-Dev <ewanmccairn2010@gmail.com>
2026-02-16 18:47:56 +0000

    feat(resize): add nearest neighbour resizing to pipeline

--- software/image-processing/imagetools.py
+++ software/image-processing/imagetools.py
@@ -1,5 +1,32 @@
 from PIL import Image
 import numpy as np
+import math
+
+    # using nearest neighbour resizing as later image processing makes interpolation negligible
+def resize(image, new_width, new_height):
+    old_height = len(image)
+    old_width= len(image[0])
+
+    # handles cases where not all sizing parameters are inputted
+    if new_width and not new_height:
+        aspect_ratio = old_width/old_height
+        new_height = int(new_width / aspect_ratio)
+    elif new_height and not new_width:
+        aspect_ratio = old_width/old_height
+        new_width = int(new_height * aspect_ratio)
+    elif not new_width and not new_height:
+        raise Exception("You need at least one sizing parameter")
+    
+
+    new_image = [[0 for _ in range(new_width)] for _ in range(new_height)] # 2d array for resized array
+
+    for y in range(new_height):
+        for x in range(new_width):
+            # scale factor * coordinate = new coordinate
+            new_x = int(x * (old_width/new_width))
+            new_y = int(y * (old_height/new_height))
+            new_image[y][x] = image[new_y][new_x]
+    return new_image
 
 def convert_image_to_RGB(path):
     img = Image.open(path)
--- software/image-processing/main.py
+++ software/image-processing/main.py
@@ -1,11 +1,18 @@
-from imagetools import preview_image, convert_image_to_RGB
+from imagetools import preview_image, convert_image_to_RGB, resize
 from greyscale import convert_RGB_to_luminosity_greyscale
 from gaussian import gaussian_blur
 from edge_detection import sobel_edge_detection
 from greyscalethreshold import greyscale_threshold
 
+
+print("Image converting to RGB")
+rgb_image = convert_image_to_RGB("/Users/ewanmccairn/Downloads/theloq.png")
+print("Done!")
+print("Resizing image...")
+resized_image = resize(rgb_image, 500, 500)
+print("Done!")
 print("Image converting to greyscale...")
-image_array = convert_RGB_to_luminosity_greyscale(convert_image_to_RGB("/Users/ewanmccairn/Downloads/Black and White Photo from Ewan McCairn.jpg"))
+image_array = convert_RGB_to_luminosity_greyscale(rgb_image)
 print("Done!")
 
 print("Blurring image...")
@@ -21,4 +28,4 @@ thresholded_array = greyscale_threshold(sobel_array, 50) # chosen threshold 50/2
 print("Done!")
 
 print("Previewing...")
-preview_image(thresholded_array)
+preview_image(resized_image)
