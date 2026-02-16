ac9a4acad3e89a4e58558d57428aa8fb51ccf7f0
Ewan-Dev <ewanmccairn2010@gmail.com>
2026-02-16 17:33:29 +0000

    feat(CLI): print out current step of image conversion pipeline

--- software/image-processing/main.py
+++ software/image-processing/main.py
@@ -4,9 +4,21 @@ from gaussian import gaussian_blur
 from edge_detection import sobel_edge_detection
 from greyscalethreshold import greyscale_threshold
 
-image_array = convert_RGB_to_luminosity_greyscale(convert_image_to_RGB("/Users/ewanmccairn/Downloads/Group 63 from Figma.png"))
-blurred_array = gaussian_blur(image_array, 0.75)
-sobel_array = sobel_edge_detection(blurred_array)
-thresholded_array = greyscale_threshold(sobel_array, 50) # chosen threshold 50/255
+print("Image converting to greyscale...")
+image_array = convert_RGB_to_luminosity_greyscale(convert_image_to_RGB("/Users/ewanmccairn/Downloads/Black and White Photo from Ewan McCairn.jpg"))
+print("Done!")
 
+print("Blurring image...")
+blurred_array = gaussian_blur(image_array, 0.75)
+print("Done!")
+
+print("Applying Sobel operator...")
+sobel_array = sobel_edge_detection(blurred_array)
+print("Done!")
+
+print("Thresholding...")
+thresholded_array = greyscale_threshold(sobel_array, 50) # chosen threshold 50/255
+print("Done!")
+
+print("Previewing...")
 preview_image(thresholded_array)
