94045a0198604cd1c41b50cf476518ec2b91ee72
Ewan-Dev <ewanmccairn2010@gmail.com>
2026-02-16 17:09:16 +0000

    feat(threshold): add threshold script for sobel output

--- software/image-processing/main.py
+++ software/image-processing/main.py
@@ -2,9 +2,11 @@ from imagetools import preview_image, convert_image_to_RGB
 from greyscale import convert_RGB_to_luminosity_greyscale
 from gaussian import gaussian_blur
 from edge_detection import sobel_edge_detection
+from greyscalethreshold import greyscale_threshold
 
-image_array = convert_RGB_to_luminosity_greyscale(convert_image_to_RGB("/Users/ewanmccairn/Downloads/Ai Animals Brainrot.jpeg"))
+image_array = convert_RGB_to_luminosity_greyscale(convert_image_to_RGB("/Users/ewanmccairn/Downloads/Group 63 from Figma.png"))
 blurred_array = gaussian_blur(image_array, 0.75)
-edges_array = sobel_edge_detection(blurred_array)
+sobel_array = sobel_edge_detection(blurred_array)
+thresholded_array = greyscale_threshold(sobel_array, 50) # chosen threshold 50/255
 
-preview_image(edges_array)
+preview_image(thresholded_array)
