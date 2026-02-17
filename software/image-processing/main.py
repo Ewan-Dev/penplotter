264cf1dce81fae68a88e2d1102483dee2b8ac18e
Ewan-Dev <ewanmccairn2010@gmail.com>
2026-02-17 18:16:43 +0000

    feat(thinning): add zhang-suen thinning algorithm

--- software/image-processing/main.py
+++ software/image-processing/main.py
@@ -3,10 +3,11 @@ from greyscale import convert_RGB_to_luminosity_greyscale
 from gaussian import gaussian_blur
 from edge_detection import sobel_edge_detection
 from greyscalethreshold import greyscale_threshold
+from zhangseunthinning import zhang_suen_thinning
 
 
 print("Image converting to RGB")
-rgb_image = convert_image_to_RGB("software/image-processing/images/rubik's.jpg")
+rgb_image = convert_image_to_RGB("software/image-processing/images/elegant-horse.webp")
 print("Done!")
 print("Resizing image...")
 resized_image = resize(rgb_image, 500, 500)
@@ -27,5 +28,9 @@ print("Thresholding...")
 thresholded_array = greyscale_threshold(sobel_array, 50) # chosen threshold 50/255
 print("Done!")
 
+print("Zhang-Suen thinning")
+thinned_image = zhang_suen_thinning(thresholded_array)
+print("Done!")
+
 print("Previewing...")
-preview_image(thresholded_array)
+preview_image(thinned_image)
---
+++ software/image-processing/zhangseunthinning.py
@@ -0,0 +1,80 @@
+def zhang_suen_thinning(image_array):
+    image_array_binary= []
+    new_image = []
+    for y in range(len(image_array)):
+        row = []
+        # zhang seun expects '0' for foregound and '1' for  background
+        for x in range(len(image_array[0])):
+            if image_array[y][x][0] < 128 and image_array[y][x][1] < 128 and image_array[y][x][2] < 128:
+                row.append(1)
+            else:
+                row.append(0)
+        image_array_binary.append(row)
+    while True:
+            to_remove = [] # stores pixels marked for removal
+            for y in range(1, len(image_array_binary) - 1):
+                for x in range(1, len(image_array_binary[0]) - 1):
+                    if image_array_binary[y][x] == 1:
+                            neighbours = [
+                                image_array_binary[y-1][x-1], image_array_binary[y-1][x], image_array_binary[y-1][x+1],
+                                image_array_binary[y][x+1], image_array_binary[y+1][x+1], image_array_binary[y+1][x],
+                                image_array_binary[y+1][x-1], image_array_binary[y][x-1]
+                            ]
+                            neighbour_count = sum(neighbours)
+                            if neighbour_count < 2 or neighbour_count > 6:
+                                continue
+                            b_w_transitions = 0
+                            for i in range (8):
+                                if neighbours[i] == 0 and neighbours[(i+1) % 8] == 1:
+                                    b_w_transitions += 1
+                            if b_w_transitions != 1:
+                                continue
+                            if neighbours[0] * neighbours[2] * neighbours[4] != 0:
+                                continue
+                            if neighbours[2] * neighbours[4] * neighbours[6] != 0:
+                                continue
+                            to_remove.append((x, y))
+
+            if not to_remove:
+                break
+            for x,y in to_remove:
+                image_array_binary[y][x] = 0
+            result = []
+            for y in range(1, len(image_array_binary) - 1):
+                for x in range(1, len(image_array_binary[0]) - 1):
+                    if image_array_binary[y][x] == 1:
+                            neighbours = [
+                                image_array_binary[y-1][x-1], image_array_binary[y-1][x], image_array_binary[y-1][x+1],
+                                image_array_binary[y][x+1], image_array_binary[y+1][x+1], image_array_binary[y+1][x],
+                                image_array_binary[y+1][x-1], image_array_binary[y][x-1]
+                            ]
+                            neighbour_count = sum(neighbours)
+                            if neighbour_count < 2 or neighbour_count > 6:
+                                continue
+                            b_w_transitions = 0
+                            for i in range (8):
+                                if neighbours[i] == 0 and neighbours[(i+1) % 8] == 1:
+                                    b_w_transitions += 1
+                            if b_w_transitions != 1:
+                                continue
+                            if neighbours[0] * neighbours[2] * neighbours[6] != 0:
+                                continue
+                            if neighbours[2] * neighbours[4] * neighbours[6] != 0:
+                                continue
+                            to_remove.append((x, y))
+
+            if not to_remove:
+                break
+            for x,y in to_remove:
+                image_array_binary[y][x] = 0
+            result = []
+    for y in range(len(image_array_binary)):
+        row = []
+        for x in range(len(image_array_binary[0])):
+            if image_array_binary[y][x] == 1:
+                row.append((0, 0, 0))
+            else:
+                row.append((255, 255, 255))
+        result.append(row)
+    
+    return result
